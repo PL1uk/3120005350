b115389efdeaf762732f6d72cf81f791117f2134
3120005350 网络工程4班 潘泽尧 <3061817763@qq.com>
2022-09-21 19:36:25 +0800

    Signed-off-by: 3120005350 网络工程4班 潘泽尧 <3061817763@qq.com>

--- ThesisCheck/src/TC/SimHash.java
+++ ThesisCheck/src/TC/SimHash.java
@@ -4,14 +4,15 @@ import java.math.BigInteger;
 import java.util.List;
 
 public interface SimHash {
-    //SimHash模块
-    BigInteger simHash();
-    //计算哈希值
-    BigInteger hash(String source);
-    //汉明距离
-    int hammingDistance(SimHashImpl other);
-    //计算汉明距离
-    double getDistance(String str1, String str2);
-    //获取特征值
-    List subByDistance(SimHashImpl simHashImpl, int distance);
+
+    BigInteger simHash();//SimHash模块
+
+    BigInteger hash(String source);//计算哈希值
+
+    int hammingDistance(SimHashImpl other);//汉明距离
+
+    double getDistance(String str1, String str2);//计算汉明距离
+
+    List subByDistance(SimHashImpl simHashImpl, int distance);//获取特征值
+
 }
